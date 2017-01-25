a5e7aa249212c4a0bf481dfd82889f6f7a61ab25
Nicolas Fernandez <nicolas_fernandez@outlook.com>
2017-01-25 21:14:36 +0100

    Update PiratesV1.py

--- PiratesV1.py
+++ PiratesV1.py
@@ -2,42 +2,45 @@ import random
 
 print ("")
 print ("You are a poor chinese slave cook on a bloodthirsty pirates ship.")
-NBPIRATES1 = int (input ("How many pirates on this ship?   "))
+NBPIRATES1 = int (input ("How many pirates on this ship?  (7)-->  "))
 
 print("")
-print ("After their last ritual,", (NBPIRATES1), "the forbans finally decided to share their magot ...")
+print ("After their last ritual,"), (NBPIRATES1), ("the forbans finally decided to share their magot ...")
 print ("The chest contains an integer x of gold coins.")
+
+print("")
 print ("They share these coins fairly, but there is a little left.")
 print ("In their great magnanimity, they decide to offer you his remaining pieces!")
-REMAIN1 = int (input ("How many coins did you receive?"))
+REMAIN1 = int (input ("How many coins did you receive?  (2)-->  "))
 
 print("")
 print ("But a mutiny broke out then, making many victims ...")
-NBPIRATES2 = NBPIRATES1 - int (input ("How many pirates died in this tragic event?   "))
+NBPIRATES2 = NBPIRATES1 - int (input ("How many pirates died in this tragic event?  (2)-->  "))
 
 print("")
 print ("The"), (NBPIRATES2), ("survivors again share fairly ALL the coins, but they still have a few more.")
 print ("In their great mansuetude, they decide to offer you again this remaining pieces!")
-REMAIN2 = int (input ("How many coins did you receive this time?   "))
+REMAIN2 = int (input ("How many coins did you receive this time?  (3)-->  "))
 
 print("")
 print ("But now a terrible storm breaks out and the ship crashes on rocks ...")
-NBPIRATES3 = int (input ("You survived! Stranded on the beach, how many pirates find you by your side?"))
+NBPIRATES3 = int (input ("You survived! Stranded on the beach, how many pirates find you by your side?  (3)-->  "))
 
 print("")
 print ("The"), (NBPIRATES3), ("remaining pirates, again share fairly ALL the pieces, but they still have a little bit of them.")
 print ("In their great kindness, they decide to offer you again this remaining pieces!")
-REMAIN3 = int (input ("How many coins did you receive after this last disbursement?   "))
+REMAIN3 = int (input ("How many coins did you receive after this last disbursement?  (2)-->  "))
 
 print("")
 print ("And while you prepare a delicious coconut turtle ragout ...")
-print ("you wonder how many gold coins you can get at least ... ")
-print ("if you poison this unfortunate survivors!")
+print ("   you wonder how many gold coins you can get at least ... ")
+print ("      if you poison this unfortunate survivors!")
 print ("")
-ANSWER = int (input ("Is that true ... How many pieces does this mysterious chest contain?"))
+ANSWER = int (input ("Is that true ... How many pieces does this mysterious chest contain?  (23)-->  "))
 
 
 LASTMODULO = NBPIRATES1 * NBPIRATES2 * NBPIRATES3
+print ("")
 print ("Congruance ="), (LASTMODULO)
 
 UNKNOW1 = NBPIRATES2 * NBPIRATES3
@@ -63,7 +66,7 @@ else:
 	print ("And no, you were wrong ... There was at least"), (TREASURE), ("gold coins in this chest ...")
 	print ("But, do you have to realize your sinister project? Only you have the answer ...")
 
-print("")
-print("Press enter key to close this game.")
+print ("")
+print ("Press enter key to close this game.")
 print ("Bye!")
-CLOSE = input ()
+print ("")
